541068fafa8825b2972675e4411e09f58e71ea2e
Khalid Khan <khankhalid00621@gamil.com>
2022-12-24 12:07:32 +0530

    Portfolio and Contact Section,Color changing mode Complete

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//---------------------------------------- typing animation---------------------------------------
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Coder"],
+    typeSpeed:80,
+    BackSpeed:60,
+    loop:true
+})
